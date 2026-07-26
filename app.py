238ca05b1963eb6b5b60db335966b7dbc48ad09a
farhadabdillahd-maker <farhadabdillahd@gmail.com>
2026-07-26 18:09:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-from home import show_home
-from upload_dataset import show_upload_dataset
-from preprocessing import show_preprocessing
-from tfidf import show_tfidf
-from klasifikasi import show_klasifikasi
-from prediksi import show_prediksi
-from about import show_about
+from modules.home import show_home
+from modules.upload_dataset import show_upload_dataset
+from modules.preprocessing import show_preprocessing
+from modules.tfidf import show_tfidf
+from modules.klasifikasi import show_klasifikasi
+from modules.prediksi import show_prediksi
+from modules.about import show_about
 
 st.set_page_config(
     page_title="Klasifikasi Tingkat Kejahatan",
@@ -41,4 +41,3 @@ elif menu == "Prediksi":
     show_prediksi()
 elif menu == "About":
     show_about()
-
